1d39f820e3ef87d36061e60aece3398c657ecb2f
joinquit3110 <riptid3110@gmail.co>
2025-04-02 23:54:36 +0700

    khoiCamOn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors({
   origin: ['https://fe-web-lilac.vercel.app'], // Chỉ cho phép frontend của bạn truy cập
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', '*'],
+  credentials: true, // Cho phép cookie
 }));
 app.use(express.json());
 
