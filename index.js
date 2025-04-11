30ff30fe623d00c1e9a2d96be2e448b8b8cc3e11
doquanghuy <admin@doitsu.tech>
2025-04-11 12:53:09 +0700

    Small Patch: Prepare for the Coming 0.4.1

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
     'http://localhost:8100',            // Ionic default port
     '*'                                 // Allow all origins in development (remove in production)
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true, // Allow cookies
   preflightContinue: false,
@@ -36,7 +36,7 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
   next();
 });
